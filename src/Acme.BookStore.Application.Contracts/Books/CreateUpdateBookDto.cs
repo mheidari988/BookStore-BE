51bb3c30827869d19bf335d2965e502f1365dbcf
Reza Heidari <m.heidari988@gmail.com>
2023-11-06 12:03:55 +0300

    Add project files.

---
+++ src/Acme.BookStore.Application.Contracts/Books/CreateUpdateBookDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Acme.BookStore.Books;
+public class CreateUpdateBookDto
+{
+    public Guid AuthorId { get; set; }
+
+    [Required]
+    [StringLength(128)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    public BookType Type { get; set; }
+
+    [Required]
+    [DataType(DataType.Date)]
+    public DateTime PublishDate { get; set; }
+
+    [Required]
+    public float Price { get; set; }
+}
---
+++ src/Acme.BookStore.Domain.Shared/Authors/AuthorConsts.cs
@@ -0,0 +1,5 @@
+﻿namespace Acme.BookStore.Authors;
+public static class AuthorConsts
+{
+    public const int MaxNameLength = 64;
+}
